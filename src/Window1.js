76bce02d78e25463e6b61ee6c69eeb927ae66a85
Ajay-Das-k <ajtechhub01@gmail.com>
2024-03-07 22:29:13 +0530

    DataNeuronAssignment1

---
+++ src/Window1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Window1 = () => {
+  return (
+    <div className="window1">
+      Window 1 Content
+    </div>
+  );
+};
+
+export default Window1
---
+++ src/Window3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Window3 = () => {
+  return (
+    <div className="window3">
+      Window 3 Content
+    </div>
+  );
+};
+
+export default Window3
